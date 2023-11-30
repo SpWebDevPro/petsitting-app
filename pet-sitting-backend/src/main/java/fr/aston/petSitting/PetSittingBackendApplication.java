f5e2fd39a20a3ec083f8ad211088ff683cada5f2
Fatima CHAMAR <fati.chamar@gmail.com>
2023-12-01 00:03:41 +0100

    First commit

---
+++ pet-sitting-backend/src/main/java/fr/aston/petSitting/PetSittingBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.aston.petSitting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSittingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSittingBackendApplication.class, args);
+	}
+
+}
